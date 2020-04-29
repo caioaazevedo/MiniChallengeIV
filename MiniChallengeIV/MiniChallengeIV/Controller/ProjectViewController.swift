ad0848ba1989ae793f954eac277c21e669bd73a7
Murilo Teixeira <murilogteixeira@gmail.com>
2020-04-29 16:38:46 -0300

    Implement project screen prototype

---
+++ MiniChallengeIV/MiniChallengeIV/Controller/ProjectViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ProjectViewController.swift
+//  MiniChallengeIV
+//
+//  Created by Murilo Teixeira on 29/04/20.
+//  Copyright © 2020 Murilo Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ProjectViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ProjectViewController: UICollectionViewDelegate {
+    
+}
+
+extension ProjectViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ProjectCollectionViewCell else {
+            return ProjectCollectionViewCell()
+        }
+        
+        cell.label.text = "Projeto \(items[indexPath.row])"
+        
+        return cell
+    }
+}
+
+extension ProjectViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth * 0.48, height: collectionViewWidth * 0.45)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
